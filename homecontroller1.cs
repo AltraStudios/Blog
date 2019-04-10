b731fb9b9fc2511d0a16c7ab392619d13c51f725
AltraStudios <44315056+AltraStudios@users.noreply.github.com>
2019-04-10 11:31:34 +0200

    Create homecontroller1.cs

---
+++ homecontroller1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Tutorials.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
