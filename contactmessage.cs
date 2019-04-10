77ea980657936614ec5d7a6343a60669a31aef5a
AltraStudios <44315056+AltraStudios@users.noreply.github.com>
2019-04-10 15:45:31 +0200

    Create contactmessage.cs

---
+++ contactmessage.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_Tutorials.Models
+{
+  public class ContactMessage
+  {
+    public string Name {get; set;}
+    public string Email { get; set; }
+    public string Message { get; set; }
+  }
+}
