5633b0c13f05d854265a42cb22a76bfa3e944407
AltraStudios <44315056+AltraStudios@users.noreply.github.com>
2019-04-14 20:25:15 +0200

    Create arraybyindex.cs

---
+++ arraybyindex.cs
@@ -0,0 +1,10 @@
+public class Program
+{
+  public static void PrintStudents(string[] student_array)
+  {
+    foreach( string studentName in student_array)
+    {
+      Console.WriteLine(studentName);
+    }
+  }
+}
